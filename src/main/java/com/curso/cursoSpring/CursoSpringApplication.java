d9e2842f86308cad129521f0f66ecb12dcc85ec1
Thiago Gandes <tgandesfreire@gmail.com>
2021-01-04 19:02:07 -0300

    produto e associacao muitos para muitos

--- src/main/java/com/curso/cursoSpring/CursoSpringApplication.java
+++ src/main/java/com/curso/cursoSpring/CursoSpringApplication.java
@@ -1,13 +1,49 @@
 package com.curso.cursoSpring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpringApplication {
+import com.curso.cursoSpring.domain.Categoria;
+import com.curso.cursoSpring.domain.Produto;
+import com.curso.cursoSpring.repositories.CategoriaRepositorie;
+import com.curso.cursoSpring.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class CursoSpringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepositorie repoCategoria;
+	@Autowired
+	private ProdutoRepository repoProduto;
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.getProdutos().add(p1);
+		cat1.getProdutos().add(p2);
+		cat1.getProdutos().add(p3);
+		
+		cat2.getProdutos().add(p2);
+		
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().add(cat1);
+		p2.getCategorias().add(cat2);
+		p3.getCategorias().add(cat1);
+		
+		repoCategoria.saveAll(Arrays.asList(cat1,cat2));
+		repoProduto.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
--- src/main/java/com/curso/cursoSpring/domain/Produto.java
+++ src/main/java/com/curso/cursoSpring/domain/Produto.java
@@ -8,28 +8,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 @Entity
-public class Categoria implements Serializable{
-
+public class Produto implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preço;
+	@ManyToMany
+	 @JoinTable(name = "PRODUTO_CATEGORIA",
+	 joinColumns = @JoinColumn(name = "produto_id"),
+	 inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	 )
+
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	@ManyToMany(mappedBy="categorias")	
-	private List<Produto> produtos = new ArrayList<>();
-	
-	public Categoria() {
-		
-	}
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preço) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preço = preço;
 	}
-	
 	public Integer getId() {
 		return id;
 	}
@@ -42,12 +46,17 @@ public class Categoria implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreço() {
+		return preço;
 	}
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreço(Double preço) {
+		this.preço = preço;
+	}
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 	@Override
 	public int hashCode() {
@@ -64,7 +73,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
